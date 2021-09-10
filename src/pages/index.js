da24617ed179d2e201afbccd90e8b93ac2b6a30d
conganh649 <tranconganh1999@gmail.com>
2021-09-10 16:15:50 +0700

    Resolve conflict & test deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import styles from "../../styles/Home.module.css";
 import Login from "./login/Login";
 
 export default function Home() {
