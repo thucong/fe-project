8a3bef9adb40e8094bde66452414df45376601c9
conganh649 <tranconganh1999@gmail.com>
2021-09-10 16:13:53 +0700

    Resolve conflict & test deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Login from "./login/Login";
 
 export default function Home() {
   async function onCallApi() {
