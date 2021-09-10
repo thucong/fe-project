f43583c8945ae7b12647b61e50a305494f5d4ffb
conganh649 <tranconganh1999@gmail.com>
2021-09-10 15:54:25 +0700

    Build login UI

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import styles from "../../styles/Home.module.css";
+import Login from "./login/Login";
+
+export default function Home() {
+  return (
+    <div>
+      <Login></Login>
+    </div>
+  );
+}
