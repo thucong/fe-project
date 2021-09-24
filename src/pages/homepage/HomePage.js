7bb5b53402b4e8d48b2a10c076a9e180ec856b30
conganh649 <tranconganh1999@gmail.com>
2021-09-24 16:40:56 +0700

    Fix header, slider size

--- src/pages/homepage/HomePage.js
+++ src/pages/homepage/HomePage.js
@@ -1,20 +1,18 @@
 import Footer from "../../components/home/footer/Footer";
 import ImageSlider from "../../components/home/carousel/ImageSlider";
 import Header from "../../components/home/header/Header";
-import ProductCard from "../../components/home/card/ProductCard";
-import product1 from "../../img/slide_one.jpg";
 const HomePage = () => {
   return (
     <div>
       <Header />
       <ImageSlider></ImageSlider>
-      <ProductCard
+      {/* <ProductCard
         productName="Vegetable"
         price="$500"
         description="We use proprietary AI to analyze the competition for your topic, and to help you create optimized content faster with state of the art AI generation."
         labels={["fresh", "vegetable", "frozen"]}
         images={product1}
-      />
+      /> */}
       <Footer />
     </div>
   );
