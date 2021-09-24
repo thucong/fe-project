fbcfbeb376e9faa13965b8151616aeec985c56d5
conganh649 <tranconganh1999@gmail.com>
2021-09-24 15:52:25 +0700

    Fix header, slider size

--- src/pages/homepage/HomePage.js
+++ src/pages/homepage/HomePage.js
@@ -1,12 +1,34 @@
 import Footer from "../../components/home/footer/Footer";
 import ImageSlider from "../../components/home/carousel/ImageSlider";
-
+import Header from "../../components/home/header/Header";
+import ProductCard from "../../components/home/card/ProductCard";
+import product1 from "../../img/testProduct1.jpg";
 const HomePage = () => {
   return (
     <div>
-      Header here
+      <Header />
       <ImageSlider></ImageSlider>
-      Footer here
+      <ProductCard
+        productName="Vegetable"
+        price="$500"
+        description="We use proprietary AI to analyze the competition for your topic, and to help you create optimized content faster with state of the art AI generation."
+        labels={["fresh", "vegetable", "frozen"]}
+        images={product1}
+      />
+      <ProductCard
+        productName="Vegetable"
+        price="$500"
+        description="We use proprietary AI to analyze the competition for your topic, and to help you create optimized content faster with state of the art AI generation."
+        labels={["fresh", "vegetable", "frozen"]}
+        images={product1}
+      />
+      <ProductCard
+        productName="Vegetable"
+        price="$500"
+        description="We use proprietary AI to analyze the competition for your topic, and to help you create optimized content faster with state of the art AI generation."
+        labels={["fresh", "vegetable", "frozen"]}
+        images={product1}
+      />
       <Footer />
     </div>
   );
