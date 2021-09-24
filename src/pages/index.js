39c59b1eec036e6dee3f4db4ced6cec4c8e31829
conganh649 <tranconganh1999@gmail.com>
2021-09-24 16:01:22 +0700

    Fix header, slider size

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import styles from "../../styles/Home.module.css";
-import Header from "../components/customer/header/Header";
 export default function Home() {
   async function onCallApi() {
     const requestOptions = {
@@ -26,8 +25,6 @@ export default function Home() {
 
   return (
     <div className={styles.container}>
-      <Header />
-
       <main className={styles.main}>
         <h1 className={styles.title}>React-project</h1>
         <button onClick={onCallApi}> Call api </button>
