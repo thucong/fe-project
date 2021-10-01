0fc6c8ab49f61030bbf882540f3296938c8c4e86
thucong <51808157+thucong@users.noreply.github.com>
2021-10-01 15:37:55 +0700

    fix image

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 export const getProducts = async () => {
   try {
-    const response = await axios.get(`${process.env.NEXT_PUBLIC_URL_API}/api/products`);
+    const response = await axios.get(
+      "https://team-project-be-dev.herokuapp.com/api/products"
+    );
     return response.data;
   } catch (err) {
     console.log(err);
