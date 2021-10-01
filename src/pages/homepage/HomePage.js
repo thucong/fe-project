88642017757300e8f582f79e7538504bbdf39dd5
thucong <51808157+thucong@users.noreply.github.com>
2021-10-01 11:24:22 +0700

    list product on home page

--- src/pages/homepage/HomePage.js
+++ src/pages/homepage/HomePage.js
@@ -1,7 +1,10 @@
 import ImageSlider from "../../components/home/carousel/ImageSlider";
 import Header from "../../components/home/header/Header";
 import Footer from "../../components/home/footer/Footer";
-const HomePage = () => {
+import ProductCard from "../../components/home/card/ProductCard";
+import {getProducts} from "../../services/ProductService";
+import styles from "./HomePage.module.css";
+const HomePage = ({products}) => {
   return (
     <div>
       <Header />
@@ -13,9 +16,21 @@ const HomePage = () => {
         labels={["fresh", "vegetable", "frozen"]}
         images={product1}
       /> */}
+        {products.map((product) => (
+      <div className={styles.card}><ProductCard key={product._id} productName={product.productName} price={product.price} description={product.description} labels={product.label} image={product.productThumbnail}/></div>
+        ))}
       <Footer />
     </div>
   );
 };
-
+export const getStaticProps = async () => {
+  const products = await getProducts();
+  //console.log(products);
+  return {
+    props: {
+      products,
+    },
+  };
+};
 export default HomePage;
+
