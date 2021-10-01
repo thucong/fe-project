ff3cdd6547c061857245bc4800019504c298aa57
thucong <51808157+thucong@users.noreply.github.com>
2021-10-01 15:20:49 +0700

    edit env

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -3,7 +3,6 @@ import axios from "axios";
 export const getProducts = async () => {
   try {
     const response = await axios.get(`${process.env.NEXT_PUBLIC_URL_API}/api/products`);
-    console.log(process.env.NEXT_PUBLIC_URL_API)
     return response.data;
   } catch (err) {
     console.log(err);
